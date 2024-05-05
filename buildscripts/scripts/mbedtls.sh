7a783298227142dfef29b782fafa9d728626524f
jmir1 <jhmiramon@gmail.com>
2024-05-05 18:43:35 +0200

    feat: Enable TLS 1.3 and add CI workflow

--- buildscripts/scripts/mbedtls.sh
+++ buildscripts/scripts/mbedtls.sh
@@ -18,5 +18,8 @@ else
 	./scripts/config.py set MBEDTLS_AESNI_C
 fi
 
+# enable TLS 1.3
+./scripts/config.py set MBEDTLS_SSL_PROTO_TLS1_3
+
 make -j$cores no_test
 make DESTDIR="$prefix_dir" install
